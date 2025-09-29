bdd69b34a547511b5a1825e102deb00fb8022d2d
Kevin Coupy <kcoupy@myges.fr>
2025-09-29 18:21:17 +0200

    asm04 v2

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -14,6 +14,9 @@ _start:
     mov rsi, buffer
     call ascii_to_int
 
+    cmp rcx, 1
+    je exit_bad_input
+
     test rax, 1
     jnz exit_odd
 
@@ -27,22 +30,34 @@ exit_odd:
     mov rdi, 1
     syscall
 
+exit_bad_input:
+    mov rax, 60
+    mov rdi, 2
+    syscall
+
 ascii_to_int:
     xor rax, rax
     xor rbx, rbx
+    xor rcx, rcx
 .loop:
     mov bl, [rsi]
     cmp bl, 10
     je .done
+    cmp bl, 0
+    je .done
+
     cmp bl, '0'
-    jl .done
+    jl .error
     cmp bl, '9'
-    jg .done
+    jg .error
 
     sub bl, '0'
     imul rax, 10
     add rax, rbx
     inc rsi
     jmp .loop
+
+.error:
+    mov rcx, 1
 .done:
     ret
